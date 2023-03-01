05c15239d0b75052ee3bba5707e7743f1a5b57fc
DiegoCh5478 <dmorales-2021420@kinal.edu.gt>
2023-02-28 21:46:44 -0600

    Se cambio el orden del schema en categoria

--- src/models/invoice.model.js
+++ src/models/invoice.model.js
@@ -5,7 +5,6 @@ const Schema = mongoose.Schema;
 
 const Invoice = Schema({
     date: {type: Date, require: true},
-    totalPrice: {type: Number, require: true},
     // Usuario al que le pertenece la factura
     ownerUser: {type: Schema.Types.ObjectId, ref: 'users'},
     //Productos de la factura
@@ -13,7 +12,8 @@ const Invoice = Schema({
         product: {type: Schema.Types.ObjectId, ref: 'products'},
         amount: {type: Number, require: true},
         prodcutNaem: {type: Number, require: true}
-    }]
+    }],
+    totalPrice: {type: Number, require: true}
 });
 
 module.exports = mongoose.model('invoices', Invoice);
